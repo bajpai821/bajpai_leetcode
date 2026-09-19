c0a7fdfec0486a226dfcc7ddde1cc397587e1975
bajpai821 <satwikbajpai464@gmail.com>
2026-09-19 22:23:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.85%) - LeetHub

--- 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -1,15 +1,40 @@
 class Solution {
 public:
     bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
-       
-        int closestX = std::max(x1, std::min(xCenter, x2));
-        int closestY = std::max(y1, std::min(yCenter, y2));
+        if(xCenter+radius>=x1 && xCenter+radius<=x2) {
+            if(yCenter+radius>=y1 && yCenter+radius<=y2) {
+                return true;
+            }
+            
+        }
+        if(xCenter-radius>=x1 && xCenter-radius<=x2) {
+            if(yCenter-radius>=y1 && yCenter-radius<=y2) {
+                return true;
+            }
+            
+        }
+        if(x1>=xCenter-radius && x1<=xCenter+radius) {
+            if(y1>=yCenter-radius && y1<=yCenter+radius) {
+                return true;
+            }
+        }
+        if(x2>=xCenter-radius && x2<=xCenter+radius) {
+            if(y2>=yCenter-radius && y2<=yCenter+radius) {
+                return true;
+            }
+        }
+        if(x1>=xCenter-radius && x1<=xCenter+radius) {
+            if(yCenter+radius>=y1 && yCenter+radius<=y2) {
+                return true;
+            }
+        }
+        if(xCenter>x1 && xCenter<x2) {
+            if(yCenter>y1-radius && yCenter<y2) {
+                return true;
+            }
+        }
+        if(y2-radius==yCenter) return true;
         
-       
-        int dx = xCenter - closestX;
-        int dy = yCenter - closestY;
-        
-       
-        return (dx * dx + dy * dy) <= (radius * radius);
+        else return false;
     }
 };
